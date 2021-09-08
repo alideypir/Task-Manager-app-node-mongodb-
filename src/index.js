feb187fe567d36ba36cd29281fc61b070ea9e5fc
ali dpr <alidaypir18@gmail.com>
2021-09-08 22:48:33 +0430

    add upload avatar and complete db

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
-
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
@@ -14,6 +13,3 @@ app.use(taskRouter);
 app.listen(port, () => {
 	console.log('Server is up on port ' + port);
 });
-
-const Task = require('./models/task');
-const User = require('./models/user');
