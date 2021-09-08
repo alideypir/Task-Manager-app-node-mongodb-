f5305fa014418caec9a037e3135ca3891bbe6ae3
ali dpr <alidaypir18@gmail.com>
2021-09-08 16:54:33 +0430

    complete all routes and models and add relation

--- src/models/task.js
+++ src/models/task.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const Task = mongoose.model('task', {
+const Task = mongoose.model('Task', {
 	description: {
 		type: String,
 		required: true,
@@ -10,6 +10,11 @@ const Task = mongoose.model('task', {
 		type: Boolean,
 		default: false,
 	},
+	owner: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true,
+		ref: 'User',
+	},
 });
 
-module.exports =  Task ;
+module.exports = Task;
